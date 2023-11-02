38010fdb8173cc9f3de26d5f65d0c6a10880674b
Jazmin Romero <jazminromero@cunet.carleton.ca>
2023-11-02 14:13:01 -0400

    RC-573 #comment topic api calls

--- maple_interface/maple_interface/maple.py
+++ maple_interface/maple_interface/maple.py
@@ -3,13 +3,14 @@ from requests import exceptions as request_exc
 
 import logging
 from maple_structures import Article
+from maple_structures import Topic
 
 logger = logging.getLogger("MapleAPI")
 
 
 class Articles:
     def __init__(
-        self, 
+        self,
         mapleapi,
         limit: int = 100,
         page: int = 0,
@@ -19,23 +20,23 @@ class Articles:
         self._limit = limit
         self._page_start = page
         self._hours = hours
-    
+
     def __iter__(self):
-        self._page=self._page_start
+        self._page = self._page_start
         # if self._page is not None:
         #     self._page +=1
         return self
-        
+
     def __next__(self):
-        articles = self._mapleapi.article_get(limit=self._limit, page=self._page, hours=self._hours)
-        
+        articles = self._mapleapi.article_get(
+            limit=self._limit, page=self._page, hours=self._hours)
+
         if isinstance(articles, requests.Response):
             raise StopIteration
         if len(articles) == 0:
             raise StopIteration
-        self._page +=1
+        self._page += 1
         return articles
-        
 
 
 class MapleAPI:
@@ -49,29 +50,32 @@ class MapleAPI:
         """baseurl is the encoded url"""
         return f"{self._authority}/{self._apiversion}"
 
-    def _post(self, path: str, headers=None, params=None, body=None, timeout = 10):
+    def _post(self, path: str, headers=None, params=None, body=None, timeout=10):
         response = requests.Response()
         try:
             url = f"{self.baseurl}/{path}"
-            response = requests.post(url, headers=headers, params=params, json=body, timeout=timeout)
+            response = requests.post(
+                url, headers=headers, params=params, json=body, timeout=timeout)
         except Exception as exc:
             logger.error(f"post: {url}. {exc}")
         return response
 
-    def _put(self, path: str, headers=None, params=None, body=None, timeout = 10):
+    def _put(self, path: str, headers=None, params=None, body=None, timeout=10):
         response = requests.Response()
         try:
             url = f"{self.baseurl}/{path}"
-            response = requests.put(url=url, headers=headers, params=params, json=body, timeout=timeout)
+            response = requests.put(
+                url=url, headers=headers, params=params, json=body, timeout=timeout)
         except Exception as exc:
             logger.error(exc)
         return response
 
-    def _get(self, path: str, headers=None, params=None, timeout = 10):
+    def _get(self, path: str, headers=None, params=None, timeout=10):
         response = requests.Response()
         try:
             url = f"{self.baseurl}/{path}"
-            response = requests.get(url=url, headers=headers, params=params, timeout=timeout)
+            response = requests.get(
+                url=url, headers=headers, params=params, timeout=timeout)
         except Exception as exc:
             logger.error(exc)
         return response
@@ -108,12 +112,12 @@ class MapleAPI:
         return response
 
     def article_get(
-        self, 
-        limit: int = None,
-        page: int = None,
-        hours: int = None,
-        url: str = None,
-        uuid: str = None):
+            self,
+            limit: int = None,
+            page: int = None,
+            hours: int = None,
+            url: str = None,
+            uuid: str = None):
         params = dict()
         if limit is not None:
             params["limit"] = limit
@@ -142,8 +146,8 @@ class MapleAPI:
     def article_iterator(self, limit: int = 100, page: int = None, hours: int = None):
         '''function to iterate through articles.'''
         return iter(Articles(
-            self, 
-            limit=limit if limit is not None else 100, 
+            self,
+            limit=limit if limit is not None else 100,
             page=page if page is not None else 0,
             hours=hours))
         # while True:
@@ -154,3 +158,49 @@ class MapleAPI:
         #     #     return articles
         #     page += 1
         #     yield articles
+
+    def topic_post(self, topic: Topic):
+        "Posts a topic in the database."
+        response = self._post("topic", params=None, body=topic.to_dict())
+
+        if response.status_code is not None:
+            if response.status_code != 201:
+                return response
+            try:
+                return Topic.from_dict(response.json())
+            except request_exc.ConnectionError as exc:
+                logger.error("No connection to backend server. %s", exc)
+            except Exception as exc:
+                if self._suppress_errors:
+                    return None
+                else:
+                    raise exc
+        else:
+            if not self._suppress_errors:
+                raise ConnectionError()
+            return {}
+
+    def topic_get(self):
+        response = self._get("topic")
+        if response.status_code != 200:
+            return response
+        else:
+            try:
+                topics_json = response.json()
+                ret = []
+                for topic_json in topics_json:
+                    ret.append(Topic.from_dict(topic_json))
+                return ret
+            except Exception as exc:
+                logger.error(exc)
+                return []
+
+    def topic_put(self, topic: Topic) -> Topic:
+        response = self._put("topic", body=topic.to_dict())
+        if response.status_code is not None:
+            if response.status_code == 200:
+                try:
+                    return Topic.from_dict(response.json())
+                except:
+                    return response
+        return response
