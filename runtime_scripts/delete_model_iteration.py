3f3334db81369afc1e6dadc8767cf40ab37cf31b
Roger Selzler <rogerselzler@cunet.carleton.ca>
2024-11-01 13:31:09 -0400

    RC-1056 #close #comment Data deletion now includes configuration from backend.

--- runtime_scripts/delete_model_iteration.py
+++ runtime_scripts/delete_model_iteration.py
@@ -1,6 +1,5 @@
 
 import logging
-import coloredlogs
 import argparse
 import enum
 import datetime
@@ -8,10 +7,9 @@ import asyncio
 import time
 import os
 import shutil
-
-from numpy import isin
-from maple_structures.model import Model, ModelIteration
+import coloredlogs
 import rcs
+from maple_structures.model import ModelIteration
 from maple_interface import MapleAPI
 from maple_config import config as cfg
 
@@ -19,18 +17,19 @@ ENV = cfg.PRODUCTION
 
 logger = logging.getLogger('ModelIterationDeletion')
 
+
 class DeleteType(enum.Enum):
     all = 'all'
     incomplete = 'incomplete'
     old = 'old'
 
 
-def delete_model_iteration(delete_type: DeleteType):
+def delete_model_iteration(delete_type: DeleteType, use_config: bool = True):
     logger.info("Delete model iterations. Deletion type: %s",
                 delete_type.value)
-    
+
     config = cfg.load_config(ENV)
-    
+
     maple = MapleAPI(
         authority=f"http://{config['MAPLE_BACKEND_IP']}:{config['MAPLE_BACKEND_PORT']}"
     )
@@ -43,15 +42,30 @@ def delete_model_iteration(delete_type: DeleteType):
             for model_iteration_ in model_iteration:
                 if isinstance(model_iteration_, ModelIteration):
                     model_iterations.append(model_iteration_)
-            
+
     logger.debug("Model iterations retrieved: %d", len(model_iterations))
     delete_model_iterations = []
 
-    
-    
     if delete_type == DeleteType.old:
         days_limit = int(config['MAPLE_MODEL_ITERATION_DATA_PERSISTENCE_DAYS'])
-        logger.info("Attempt deletion of model iterations older than %d days", days_limit)
+        if use_config:
+            backend_config = maple.config_get()
+            if isinstance(backend_config, dict):
+                if 'model_iteration_persistence_days' in backend_config:
+                    days_limit = int(
+                        backend_config['model_iteration_persistence_days'])
+                else:
+                    logger.error(
+                        "Backend configuration does not have 'model_iteration_persistence_days'")
+                    return
+            else:
+                logger.error(
+                    "Failed to fetch backend configuration. Cancelling deletion.")
+                return
+
+        logger.info(
+            "Attempt deletion of model iterations older than %d days", days_limit)
+
     for model_iteration in model_iterations:
         if delete_type == DeleteType.old:
             datediff = datetime.datetime.now(datetime.timezone.utc)-datetime.datetime.fromisoformat(
@@ -64,7 +78,7 @@ def delete_model_iteration(delete_type: DeleteType):
             models = [
                 getattr(model_iteration, f'model_level{level}') for level in range(1, 4)]
             complete_models = [True if model.status ==
-                            'complete' else False for model in models]
+                               'complete' else False for model in models]
             if not all(complete_models):
                 delete_model_iterations.append(model_iteration)
 
@@ -87,41 +101,59 @@ def delete_model_iteration(delete_type: DeleteType):
                 model_iteration.uuid)
             if os.path.exists(model_iteration_data_path):
                 try:
-                    logger.debug('Removing files associated with model iteration %s', model_iteration.uuid)
+                    logger.debug(
+                        'Removing files associated with model iteration %s',
+                        model_iteration.uuid)
                     shutil.rmtree(model_iteration_data_path)
                 except Exception as exc:
-                    logger.error('Failed to remove directory for model iteration %s. %s', model_iteration.uuid, exc)
-                    logger.info('Model iteration %s will not be deleted.', model_iteration.uuid)
+                    logger.error(
+                        'Failed to remove directory for model iteration %s. %s',
+                        model_iteration.uuid,
+                        exc)
+                    logger.info(
+                        'Model iteration %s will not be deleted.',
+                        model_iteration.uuid)
                     continue
             else:
-                logger.debug('Model iteration %s does not have data. Skip data deletion.', model_iteration.uuid)
-            
+                logger.debug(
+                    'Model iteration %s does not have data. Skip data deletion.',
+                    model_iteration.uuid)
+
             if os.path.isfile(f"{model_iteration_data_path}.zip"):
                 os.remove(f"{model_iteration_data_path}.zip")
-                        
+
             # remove from backend
             response = maple.model_iteration_delete(model_iteration.uuid)
             if response != 200:
                 logger.error(
-                    'Failed deletion of model_iteration with uuid: %s. %s', model_iteration.uuid, response)
+                    'Failed deletion of model_iteration with uuid: %s. %s',
+                    model_iteration.uuid,
+                    response)
 
         except Exception as exc:
             logger.error('Failed to remove model iteration %s.', exc)
 
 
-async def delete_model_iteration_async(delete_type: DeleteType, period_seconds: int = None):
+async def delete_model_iteration_async(
+        delete_type: DeleteType,
+        period_seconds: int = None,
+        use_config: bool = True):
+
     tstart = time.time()
     while True:
         try:
-            delete_model_iteration(delete_type=delete_type)
+            delete_model_iteration(
+                delete_type=delete_type, use_config=use_config)
             if period_seconds is None:
                 wait_time = rcs.utils.time_to_midnight()
             else:
-                wait_time = period_seconds - ((time.time()-tstart)%period_seconds)
+                wait_time = period_seconds - \
+                    ((time.time()-tstart) % period_seconds)
                 if wait_time < 0:
                     wait_time = 0
                 logger.debug("wait_time is %f", wait_time)
-            logger.info('Next deletion will occur in %.2f hours', wait_time/60/60)
+            logger.info('Next deletion will occur in %.2f hours',
+                        wait_time/60/60)
             await asyncio.sleep(wait_time)
         except Exception as exc:
             logger.error('Failed to remove model iteration. %s', exc)
@@ -151,6 +183,13 @@ parser.add_argument(
     default='info',
     choices=['debug', 'info', 'warning', 'error', 'critical'],
 )
+parser.add_argument(
+    '-c',
+    '--use_config',
+    action='store_true',
+    help='''If provided, the configuration will be fetched from backend and used 
+    for deletion in case type is old''',
+)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -162,21 +201,28 @@ if __name__ == '__main__':
         n_log_files=1,
         use_postfix_hour=False,
     )
-    
+
     coloredlogs.install(level=getattr(logging, args.l.upper()))
-    
+
     logger.debug('Arguments provided are: %s', args)
-    
+
     delete_type = getattr(DeleteType, args.t)
-    
+
     if args.a:
         if not args.p:
-            logger.info('Period not provided. Deletion will occur every midnight.')
+            logger.info(
+                'Period not provided. Deletion will occur every midnight.')
         if delete_type != DeleteType.old:
-            raise ValueError("In async (a) mode, only 'old' deletion type is accepted.")
+            raise ValueError(
+                "In async (a) mode, only 'old' deletion type is accepted.")
         logger.debug("Running script in async mode with period %d.", args.p)
         asyncio.run(
-            delete_model_iteration_async(delete_type=delete_type, period_seconds=args.p)
+            delete_model_iteration_async(
+                delete_type=delete_type,
+                period_seconds=args.p,
+                use_config=args.use_config)
         )
     else:
-        delete_model_iteration(delete_type=delete_type)
+        delete_model_iteration(
+            delete_type=delete_type,
+            use_config=args.use_config)
